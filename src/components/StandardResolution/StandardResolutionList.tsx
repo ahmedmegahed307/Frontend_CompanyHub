56cf5c7751ca95cb23308f03ddd8ea6991d35d69
bariq alshakkaki <bariq1991@hotmail.com>
2023-05-23 16:42:43 +0300

    res

--- src/components/StandardResolution/StandardResolutionList.tsx
+++ src/components/StandardResolution/StandardResolutionList.tsx
@@ -29,34 +29,106 @@ import {
   AbsoluteCenter,
   FormControl,
   FormLabel,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
 } from "@chakra-ui/react";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { DataStore } from "aws-amplify";
 import { Resolutions, UsersObject } from "../../models";
 import { NavLink } from "react-router-dom";
 import { AddIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import Swal from "sweetalert2";
+import React from "react";
+
 const StandardResolutionList = () => {
   const [resolutionList, setResolutionLists] = useState<Resolutions[]>();
   const [createResolution, setResolution] = useState({
     name: "",
   });
 
+
+  const [editResolution1, setEditResolution1] = useState({
+    id: "",
+    name: "",
+    isActive:false
+  });
+
+
+  const [editResolution, setEditResolution] = useState<Resolutions>(
+    new Resolutions({ name: "" })
+  );
+
   const handleCreate = async (event: FormEvent) => {
     event.preventDefault();
-    console.log(createResolution);
 
-    const post = await DataStore.save(
-      new Resolutions({isActive:true,name:createResolution.name})
-    );
+    try {
+      console.log(createResolution);
 
-    onClose();
+      const post = await DataStore.save(
+        new Resolutions({ isActive: true, name: createResolution.name })
+      );
 
-    setResolution({ name: "" });
+      Swal.fire({
+        title: "Congratulations",
+        text: "Resolutions have been saved successfully",
+        icon: "success",
+      });
+      createModal.onClose();
+
+      setResolution({ name: "" });
+    } catch (error: any) {
+      Swal.fire({
+        title: "Oops",
+        text: error,
+        icon: "error",
+      });
+    }
   };
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleUpdate = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      console.log(editResolution);
+      const original = await DataStore.query(Resolutions, editResolution1!.id);
+
+      if (original) {
+        const updatedPost = await DataStore.save(
+          Resolutions.copyOf(original, (updated) => {
+            updated.name=editResolution1.name,
+            updateModal.onClose();
+            Swal.fire({
+              title: "Congratulations",
+              text: "Resolutions have been saved successfully",
+              icon: "success",
+            });
+          })
+        );
+      }
+
+      setResolution({ name: "" });
+    } catch (error: any) {
+      Swal.fire({
+        title: "Oops",
+        text: error,
+        icon: "error",
+      });
+    }
+  };
+
+  const createModal = useDisclosure();
+  const deleteModal = useDisclosure();
+  const updateModal = useDisclosure();
+  const cancelRef = useRef<HTMLButtonElement>(null);
   useEffect(() => {
-    const lists = DataStore.observeQuery(Resolutions).subscribe(({ items }) => {
+    const lists = DataStore.observeQuery(Resolutions, (c) =>
+      c.isActive.eq(true)
+    ).subscribe(({ items }) => {
       setResolutionLists(items);
     });
 
@@ -86,7 +158,7 @@ const StandardResolutionList = () => {
 
           <Button
             onClick={() => {
-              onOpen();
+              createModal.onOpen();
             }}
             leftIcon={<AddIcon />}
             my={10}
@@ -126,7 +198,10 @@ const StandardResolutionList = () => {
                                 aria-label="Search database"
                                 as={NavLink}
                                 icon={<EditIcon />}
-                                onClick={() => {}}
+                                onClick={() => {
+                                  setEditResolution1({id:resolution.id,name:resolution.name,isActive:true});
+                                  updateModal.onOpen();
+                                }}
                                 colorScheme="blue"
                                 variant={"solid"}
                                 size={"sm"}
@@ -137,7 +212,10 @@ const StandardResolutionList = () => {
                                 aria-label="Search database"
                                 as={NavLink}
                                 icon={<DeleteIcon />}
-                                onClick={() => {}}
+                                onClick={() => {
+                                  setEditResolution(resolution);
+                                  deleteModal.onOpen();
+                                }}
                                 colorScheme="blue"
                                 variant={"solid"}
                                 size={"sm"}
@@ -154,8 +232,12 @@ const StandardResolutionList = () => {
           </TabPanels>
         </Tabs>
       </Flex>
-
-      <Drawer onClose={onClose} isOpen={isOpen} size={"lg"}>
+      {/* create */}
+      <Drawer
+        onClose={createModal.onClose}
+        isOpen={createModal.isOpen}
+        size={"lg"}
+      >
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
@@ -196,6 +278,99 @@ const StandardResolutionList = () => {
           </DrawerBody>
         </DrawerContent>
       </Drawer>
+
+      {/* Update modal  */}
+
+      <Drawer
+        onClose={updateModal.onClose}
+        isOpen={updateModal.isOpen}
+        size={"lg"}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+
+          <DrawerBody>
+            <AbsoluteCenter>
+              <form onSubmit={handleUpdate}>
+                <Heading my={5} size={"md"}>
+                  Update Resolution
+                </Heading>
+                <FormControl pb={5} w={"lg"}>
+                  <FormLabel>Name</FormLabel>
+                  <Input
+                    className="FormControl"
+                    placeholder=""
+                    value={editResolution1!.name}
+                    onChange={(e) =>
+                      setEditResolution1({
+                        ...editResolution1!,
+                        name: e.target.value,
+                      })
+                    }
+                    required
+                  />
+                </FormControl>
+
+                <Button
+                  type="submit"
+                  colorScheme="blue"
+                  w={"full"}
+                  bg={"#294c58"}
+                  my={10}
+                >
+                  Create
+                </Button>
+              </form>
+            </AbsoluteCenter>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+      {/* Delete Modal  */}
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={deleteModal.onClose}
+        isOpen={deleteModal.isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>Discard Changes?</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            Are you sure you want to discard all of your notes? 44 words will be
+            deleted.
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={deleteModal.onClose}>
+              No
+            </Button>
+            <Button
+              colorScheme="red"
+              onClick={async () => {
+                const original = await DataStore.query(
+                  Resolutions,
+                  editResolution!.id
+                );
+
+                if (original) {
+                  const updatedPost = await DataStore.save(
+                    Resolutions.copyOf(original, (updated) => {
+                      updated.isActive = false;
+                      deleteModal.onClose();
+                    })
+                  );
+                }
+              }}
+              ml={3}
+            >
+              Yes
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 };
