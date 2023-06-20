11873ccab9582db1bf3eeb2a860ff7b7c8fa1593
ahmedm <ahmed.mucahit@afsgo.com>
2023-06-20 14:29:20 +0300

    added reactQuery to jobtype

---
+++ src/hooks/Settings/JobType/useJobType.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { DataStore } from "aws-amplify";
+import { JobTypesList } from "../../../models";
+
+const useJobType= () =>{
+
+    return  useQuery<JobTypesList[],Error>({
+        queryKey: ['jobTypeList'],
+        queryFn: () => {
+          return DataStore.query(JobTypesList, (c) => c.isActive.eq(true));
+        },
+      });
+}
+export default useJobType;
